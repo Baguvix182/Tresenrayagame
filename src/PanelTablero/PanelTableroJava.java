5a8b37b6db56b7999a6cab6fbb4ee5379c41e75b
unknown <tu_correo@ejemplo.com>
2026-06-01 15:11:50 -0600

    se agrego colores

--- src/PanelTablero/PanelTableroJava.java
+++
@@ -1,5 +0,0 @@
-package PanelTablero;
-
-public class PanelTableroJava {
-
-}
--- src/trabajofinalmodulo/Colores.java
+++ src/trabajofinalmodulo/Colores.java
@@ -1,5 +1,35 @@
 package trabajofinalmodulo;
 
+import java.awt.Color;
+
 public class Colores {
+
+    // Fondo principal
+    public static final Color BACKGROUND = new Color(30, 30, 30);
+
+    // Fondo de celdas
+    public static final Color CELL_BACKGROUND = new Color(13, 13, 40);
+
+    // Color de la cuadrícula
+    public static final Color GRID_COLOR = new Color(220, 80, 220);
+
+    // Jugador X
+    public static final Color PLAYER_X = new Color(0, 200, 255);
+
+    // Jugador O
+    public static final Color PLAYER_O = new Color(220, 80, 220);
+
+    // Color ganador
+    public static final Color WIN_COLOR = new Color(0, 255, 160);
+
+    // Avatar X
+    public static final Color AVATAR_X = new Color(0, 200, 255);
+
+    // Avatar O
+    public static final Color AVATAR_O = new Color(220, 80, 220);
+
+    // Texto
+    public static final Color TEXT_WHITE = new Color(240, 240, 255);
+    public static final Color TEXT_GRAY  = new Color(160, 160, 180);
 
 }
